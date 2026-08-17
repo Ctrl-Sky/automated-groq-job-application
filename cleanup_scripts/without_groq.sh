7c0ef26b8fa5780fe3237150a7b0b9a9e9050bf1
Sky <81098308+Ctrl-Sky@users.noreply.github.com>
2026-08-17 15:37:49 -0400

    add changes to reflect new application process

---
+++ cleanup_scripts/without_groq.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+COMPANY_NAME="$1"
+JOB_TITLE="$2"
+
+MONTH_DATE=$(date +"%b-%d-%Y")
+DIR_PATH="outputs/$MONTH_DATE/$COMPANY_NAME-$JOB_TITLE"
+INITIALS="${COMPANY_NAME:0:1}${JOB_TITLE:0:1}"
+
+# If dir does not exist, create it
+if [ ! -d "$DIR_PATH" ]; then
+    echo "Creating Directory"
+    mkdir -p "$DIR_PATH"
+fi
+
+# Copy Job Description to path
+echo "Moving job description"
+cp inputs/job_description.txt "$DIR_PATH"
+
+# # Rename and move cover letter
+# echo "Moving Cover Letter"
+# mv cover_letter.docx "Sky_Quan_Cover_Letter_$INITIALS.docx"
+# mv "Sky_Quan_Cover_Letter_$INITIALS.docx" "$DIR_PATH"
+
+# # Rename and move resume
+# echo "Moving Resume"
+# mv resume.docx "Sky_Quan_Resume_$INITIALS.docx"
+# mv "Sky_Quan_Resume_$INITIALS.docx" "$DIR_PATH"
+
+# # Copy original resume
+# cp "resumes/Sky_Quan_Resume.docx" "$DIR_PATH"
+
+echo "Script completed Successfully"
---
+++ run_without.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Keep up to date
+git pull
+
+# Install Dependencies
+python3.12 -m venv .venv
+. .venv/bin/activate
+pip install -r requirements.txt
+
+# Get variables from job_description.txt
+export COMPANY_NAME=$(python3.12 utils/get_company_name.py)
+export JOB_TITLE=$(python3.12 utils/get_job_title.py)
+
+# Generate Resume and Cover Letter
+# python3.12 src/main.py
+
+# Store and save resume and cover letter
+bash cleanup_scripts/without_groq.sh "${COMPANY_NAME}" "${JOB_TITLE}"
+# python3.12 cleanup_scripts/save_to_csv.py "${COMPANY_NAME}" "${JOB_TITLE}"
+
+echo "Application successfully executed"
+
+# Break down
+deactivate
+git add .
+git commit -m "(Automated Commit) Job application updated"
+git push
